57829db2b361d0485698af6300c19b9f3f6634eb
AftabAhmedFahim <aftabahmed.official004@gmail.com>
2025-09-14 23:43:02 +0600

    Added Hashing

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
   fullName: { type: String, required: true, trim: true },
@@ -32,5 +33,21 @@ const userSchema = new mongoose.Schema({
     } 
   }
 }, { timestamps: true });
+
+// Hashing Password for safety
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
 
 export default mongoose.model("User", userSchema);
